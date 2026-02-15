fa351c82e58aaff5da142f0cb706e666835ec0fd
Vitor Monteiro <vitor.azevedomonteiro@iusspavia.it>
2026-02-15 15:44:44 +0100

    Update ex

--- example.py
+++ example.py
@@ -3,17 +3,17 @@ Author: Vitor Monteiro, IUSS-Pavia, 2025 PhD Candidate, 18/09/2025.
 
 
 Compute cross-spatial correlation coefficients using Principal Component Analysis
-(PCA) for Sa(T*), PGA, PGV, Saavg2(T**), Saavg3(T**), and FIV3(T**) for NGA-West2
-and ESM databases for T* = [0.01-5.0]s and T** = [0.1-4.0]s.
+(PCA) for Sa(T*), PGA, PGV, Saavg2(T**), Saavg3(T***), and FIV3(T****) for NGA-West2
+and ESM databases for T* = [0.01-5.0]s, T** = [0.1-3.0]s, T*** = [0.1-2.0], and
+T**** = [0.1-4.0]
 
 For more details please see: 
-Monteiro, V.A, Aristeidou, S. and O’Reilly, G.J. (2025) ‘Modelling Cross-Spatial
-Correlation for Amplitude and Next-Generation Intensity Measures using principal
-component analysis and geostatistics for active shallow crustal zones’ 
+Monteiro, V.A, Aristeidou, S. and O’Reilly, G.J. (2026) ‘Spatial cross-correlation
+models for next-generation amplitude and cumulative intensity measures’ 
 (under review)
 
 
----------------  MAO2025 function  ---------------
+---------------  MAO2026 function  ---------------
 
 Arguments:
     IM1 (float): First intensity measure (e.g., ground motion parameter).
@@ -34,12 +34,14 @@ Returns:
                     
 IMs available are 'Sa', 'Saavg2', 'Saavg3', 'FIV3', 'PGA', and 'PGV'.
 For Sa the range of periods is [0.01, 5.0]s
-For Saavg2, Saavg3, and FIV3 the range of periods is [0.1, 4.0]s
+For Saavg2 the range of periods is [0.1, 3.0]s,
+For Saavg3 the range of periods is [0.1, 2.0]s,
+For FIV3 the range of periods is [0.1, 4.0]s
 
 """
 
 
-from GetSpatialCorrelation import MAO2025
+from GetSpatialCorrelation import MAO2026
 
 # Example
 IM1 = "FIV3(0.6)"
@@ -47,7 +49,8 @@ IM2 = "FIV3(0.6)"
 import numpy as np
 h_distance = np.linspace(0,150,1500) #km
 for h in h_distance:
-    corr = MAO2025(IM1, IM2, h, cluster=0, vs30=2)
+    corr = MAO2026(IM1, IM2, h, cluster=0, vs30=2)
     print(corr)
 
 #print(f'Spatial Correlation between {IM1} and {IM2} at {h_distance} km is: {corr:.4f}')
+
